0f10f133ba15529dc428b978900eeb2289c7f401
sanjanavamja18 <sanjana.vamja@exhibytesolution.com>
2022-08-24 18:32:03 +0530

    Login-page

---
+++ login-cookie/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Login from "./component/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Login />
+      </div>
+    </div>
+  );
+}
+
+export default App;
