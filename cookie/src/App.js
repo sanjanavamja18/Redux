53479bb766200bd16b73bb572e8a28c18b6732dd
sanjanavamja18 <sanjana.vamja@exhibytesolution.com>
2022-08-25 18:48:37 +0530

    Cookie

--- cookie/src/App.js
+++ cookie/src/App.js
@@ -1,9 +1,35 @@
-function App() {
+import React, { useState } from "react";
+import { useCookies } from "react-cookie";
+
+const App = () => {
+  const [name, setName] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [cookies, setCookie] = useCookies(["user"]);
+
+  const handle = () => {
+    setCookie("Name", name, { path: "/" });
+    setCookie("Password", pwd, { path: "/" });
+  };
   return (
     <div className="App">
-      <h1>login</h1>
+      <h1>Name:</h1>
+      <input
+        placeholder="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <h1>Password:</h1>
+      <input
+        type="password"
+        placeholder="password"
+        value={pwd}
+        onChange={(e) => setPwd(e.target.value)}
+      />
+      <br></br>
+      <div>
+        <button onClick={handle}>Set Cookie</button>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
--- cookie/src/index.js
+++ cookie/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-
+import ReactDOM from "react-dom";
+import { CookiesProvider } from "react-cookie";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <CookiesProvider>
     <App />
-  </React.StrictMode>
+  </CookiesProvider>,
+  document.getElementById("root")
 );
