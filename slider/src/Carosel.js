43ebb82fd47add9eda57c548135207a0e122e119
sanjanavamja18 <sanjana.vamja@exhibytesolution.com>
2022-08-29 18:48:52 +0530

    Slick-slider

--- slider/src/Carosel.js
+++ slider/src/Carosel.js
@@ -4,9 +4,12 @@ import "slick-carousel/slick/slick-theme.css";
 import image1 from "./assets/img1.jpeg";
 import image2 from "./assets/img2.jpeg";
 import image3 from "./assets/img3.jpeg";
-import "./Carousel.css";
+import "./Carosel.css";
+import { useState } from "react";
 
 const Carousel = () => {
+  const [current, setCurrent] = useState("0");
+
   return (
     <div style={{ margin: "30px" }}>
       <h1>Basic Slick</h1>
