457e1e38d996db41345495d14758ba03bb775a6e
sanjanavamja18 <sanjana.vamja@exhibytesolution.com>
2022-08-30 18:50:10 +0530

    Slick-slider

--- react-slick/src/App.js
+++ react-slick/src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-
+import Slideshow from "./Sildeshow";
 function App() {
   return (
     <div className="App">
-      <h2>React-slick</h2>
+      <Slideshow />
     </div>
   );
 }
