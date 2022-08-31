c1da04bbcaccfe7a637763690f10b1afd8fea57d
sanjanavamja18 <sanjana.vamja@exhibytesolution.com>
2022-08-31 18:29:02 +0530

    React-Slick

---
+++ react-slick/src/Img1.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Img1 = () => {
+  return (
+    <div>
+      <img src="D:\Sanjana\Redux\react-slick\src\assets\img1.jpeg" />
+    </div>
+  );
+};
+
+export default Img1;
---
+++ react-slick/src/Img3.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Img3 = () => {
+  return (
+    <div>
+      <img src="D:\Sanjana\Redux\react-slick\src\assets\img3.jpeg" />
+    </div>
+  );
+};
+
+export default Img3;
